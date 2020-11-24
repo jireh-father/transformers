c16af4d734ef855d16a3729f2cf683819a41409a
seoilgun <seoilgun@gmail.com>
2020-11-24 20:02:26 +0900

    a

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -113,6 +113,8 @@ class BaseTransformer(pl.LightningModule):
                 self.hparams.tokenizer_name if self.hparams.tokenizer_name else self.hparams.model_name_or_path,
                 cache_dir=cache_dir,
             )
+            print(self.hparams.tokenizer_name if self.hparams.tokenizer_name else self.hparams.model_name_or_path)
+            print(cache_dir)
             print("tokenizer", self.tokenizer)
         else:
             self.tokenizer: PreTrainedTokenizer = tokenizer
