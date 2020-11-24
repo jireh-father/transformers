cb27d2f3dae83e540279f9f4670d37f1f5e7e28d
seoilgun <seoilgun@gmail.com>
2020-11-24 11:30:08 +0900

    a

--- examples/lightning_base.py
+++ examples/lightning_base.py
@@ -117,18 +117,18 @@ class BaseTransformer(pl.LightningModule):
             self.tokenizer: PreTrainedTokenizer = tokenizer
         self.model_type = MODEL_MODES[mode]
         if model is None:
-            # self.model = self.model_type.from_pretrained(
-            #     self.hparams.model_name_or_path,
-            #     from_tf=bool(".ckpt" in self.hparams.model_name_or_path),
-            #     config=self.config,
-            #     cache_dir=cache_dir,
-            # )
-            self.model = self.model_type.from_config(
-                # self.hparams.model_name_or_path,
-                # from_tf=bool(".ckpt" in self.hparams.model_name_or_path),
+            self.model = self.model_type.from_pretrained(
+                self.hparams.model_name_or_path,
+                from_tf=bool(".ckpt" in self.hparams.model_name_or_path),
                 config=self.config,
-                # cache_dir=cache_dir,
+                cache_dir=cache_dir,
             )
+            # self.model = self.model_type.from_config(
+            #     # self.hparams.model_name_or_path,
+            #     # from_tf=bool(".ckpt" in self.hparams.model_name_or_path),
+            #     config=self.config,
+            #     # cache_dir=cache_dir,
+            # )
             print(self.model)
         else:
             self.model = model
