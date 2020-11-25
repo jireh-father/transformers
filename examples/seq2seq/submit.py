9d1eedd0195e578e3788fbbb3489272f25f85119
seoilgun <seoilgun@gmail.com>
2020-11-25 18:57:36 +0900

    a

---
+++ examples/seq2seq/submit.py
@@ -0,0 +1,39 @@
+import argparse
+import random
+import jsonlines
+import os
+import csv
+
+
+def main(args):
+    for arg in vars(args):
+        print(arg, getattr(args, arg))
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    summaries = open(args.generated_file).readlines()
+    ids = []
+    with jsonlines.open(args.test_file) as f:
+        for i, line in enumerate(f.iter()):
+            ids.append(line['id'])
+
+    rows = zip(ids, summaries)
+    with open(os.path.join("submission.csv"), "w+") as f:
+        writer = csv.writer(f)
+        writer.writerow(["id", "summary"])
+        for row in rows:
+            writer.writerow(row)
+    print("done")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--generated_file', type=str,
+                        default='/media/irelin/data_disk/dataset/dacon_summury/abstractive/abstractive_test_v2.jsonl')
+    parser.add_argument('--test_file', type=str,
+                        default='/media/irelin/data_disk/dataset/dacon_summury/abstractive/abstractive_test_v2.jsonl')
+    parser.add_argument('--output_dir', type=str,
+                        default='/media/irelin/data_disk/dataset/dacon_summury/abstractive/preprocessed')
+
+    main(parser.parse_args())
