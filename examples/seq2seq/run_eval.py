5d1f6b8e4fa4a3eacd8ab827c1b98ebacfde1364
seoilgun <seoilgun@gmail.com>
2020-11-25 11:22:49 +0900

    a

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -67,7 +67,8 @@ def generate_summaries_or_translations(
     for examples_chunk in tqdm(list(chunks(examples, batch_size))):
         examples_chunk = [prefix + text for text in examples_chunk]
         batch = tokenizer(examples_chunk, return_tensors="pt", truncation=True, padding="longest").to(device)
-        print(batch.shape)
+        print(len(batch))
+        print(batch[0].shape)
         summaries = model.generate(
             input_ids=batch.input_ids,
             attention_mask=batch.attention_mask,
