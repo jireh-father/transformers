f271bc00283acdedc0a5b0fb984a13f617010b41
seoilgun <seoilgun@gmail.com>
2020-11-26 10:01:56 +0900

    a

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -72,8 +72,14 @@ def generate_summaries_or_translations(
         summaries = model.generate(
             input_ids=batch.input_ids,
             attention_mask=batch.attention_mask,
-            **generate_kwargs,
+            do_sample=True,  # 샘플링 전략 사용
+            max_length=50,  # 최대 디코딩 길이는 50
+            top_k=50,  # 확률 순위가 50위 밖인 토큰은 샘플링에서 제외
+            top_p=0.95,  # 누적 확률이 95%인 후보집합에서만 생성
+            num_return_sequences=3  # 3개의 결과를 디코딩해낸다
+            # **generate_kwargs,
         )
+        print(summaries)
         dec = tokenizer.batch_decode(summaries, skip_special_tokens=True, clean_up_tokenization_spaces=False)
         for hypothesis in dec:
             print(hypothesis)
