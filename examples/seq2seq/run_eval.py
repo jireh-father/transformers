9c064b12df483f48bb69573eb90d68982ce1666e
seoilgun <seoilgun@gmail.com>
2020-11-26 10:05:42 +0900

    a

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -76,7 +76,7 @@ def generate_summaries_or_translations(
             max_length=50,  # 최대 디코딩 길이는 50
             top_k=50,  # 확률 순위가 50위 밖인 토큰은 샘플링에서 제외
             top_p=0.95,  # 누적 확률이 95%인 후보집합에서만 생성
-            num_return_sequences=3  # 3개의 결과를 디코딩해낸다
+            num_return_sequences=1  # 3개의 결과를 디코딩해낸다
             # **generate_kwargs,
         )
         print(summaries)
