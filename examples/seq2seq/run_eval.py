88f59657fe3e31e24b2cf57a8ca7047caa4a57d4
seoilgun <seoilgun@gmail.com>
2020-11-26 10:11:28 +0900

    a

--- examples/seq2seq/run_eval.py
+++ examples/seq2seq/run_eval.py
@@ -73,7 +73,7 @@ def generate_summaries_or_translations(
             input_ids=batch.input_ids,
             attention_mask=batch.attention_mask,
             do_sample=True,  # 샘플링 전략 사용
-            max_length=50,  # 최대 디코딩 길이는 50
+            max_length=128,  # 최대 디코딩 길이는 50
             top_k=50,  # 확률 순위가 50위 밖인 토큰은 샘플링에서 제외
             top_p=0.95,  # 누적 확률이 95%인 후보집합에서만 생성
             num_return_sequences=1  # 3개의 결과를 디코딩해낸다
