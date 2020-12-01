daaa106f70b6ba33ee8987af5224ec9a401a7ebf
seoilgun <seoilgun@gmail.com>
2020-12-01 17:56:56 +0900

    a

--- examples/seq2seq/utils.py
+++ examples/seq2seq/utils.py
@@ -417,7 +417,7 @@ logger = getLogger(__name__)
 def use_task_specific_params(model, task):
     """Update config with summarization specific params."""
     task_specific_params = model.config.task_specific_params
-
+    print('task_specific_params', task_specific_params)
     if task_specific_params is not None:
         pars = task_specific_params.get(task, {})
         logger.info(f"using task specific params for {task}: {pars}")
